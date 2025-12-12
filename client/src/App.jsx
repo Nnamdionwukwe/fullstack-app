ef070c587d764895fa563e7c6d02f0c2dc7a4b75
Onwukwennamdi <nnamdionwukwe@gmail.com>
2025-12-12 18:25:28 +0100

    de

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   // Define the base URL of your deployed Vercel backend
   // !!! IMPORTANT: REPLACE THIS WITH YOUR ACTUAL DOMAIN !!!
-  const API_BASE_URL = "https://fullstack-app-ueic.vercel.app";
+  const API_BASE_URL = "https://fullstack-app-wddq.vercel.app";
 
   useEffect(() => {
     const fetchProducts = async () => {
